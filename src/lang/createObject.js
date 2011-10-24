7eed35a132ae9b659279fec0f44f20d53ffef3d9
millermedeiros <miller@millermedeiros.com>
2011-10-24 12:21:09 -0200

    start documentation.

--- src/lang/createObject.js
+++ src/lang/createObject.js
@@ -4,8 +4,8 @@ define(['./mixIn'], function(mixIn){
      * Create Object using prototypal inheritance and setting custom properties.
      * - Mix between Douglas Crockford Prototypal Inheritance <http://javascript.crockford.com/prototypal.html> and the EcmaScript 5 `Object.create()` method.
      * @param {object} parent    Parent Object.
-     * @param {object} props Object properties.
-     * @return {object} cloned Object.
+     * @param {object} [props] Object properties.
+     * @return {object} Created object.
      * @version 0.1.0 (2011/08/09)
      * @author Miller Medeiros
      */
