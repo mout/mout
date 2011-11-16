0b5aeaf453c7dea8114b6853489b463b1561592b
millermedeiros <miller@millermedeiros.com>
2011-11-16 17:05:15 -0200

    simplify math/randInt logic. closes #8.

--- src/math/randInt.js
+++ src/math/randInt.js
@@ -1,22 +1,16 @@
-define(['../number/MIN_INT', '../number/MAX_INT', '../number/toInt', './countSteps'], function(MIN_INT, MAX_INT, toInt, countSteps){
+define(['../number/MIN_INT', '../number/MAX_INT', '../number/toInt', './rand'], function(MIN_INT, MAX_INT, toInt, rand){
 
     /**
      * Gets random integer inside range or snap to min/max values.
-     * @version 0.3.0 (2011/11/15)
-     * @author Miller Medeiros
+     * @version 0.4.0 (2011/11/16)
+     * @author Miller Medeiros / Fabio Caccamo
      */
     function randInt(min, max, shouldSnap){
         min = min == null? MIN_INT : toInt(min);
         max = max == null? MAX_INT : toInt(max);
-
-        var rnd = Math.random(),
-            diff = max - min;
-
-        if (shouldSnap || diff < 2) {
-            return rnd < 0.5? min : max;
-        }
-
-        return min + countSteps(rnd,  1 / (diff + 1));
+        // can't be max + 0.5 otherwise it will round up if `rand`
+        // returns `max` causing it to overflow range.
+        return Math.round( rand(min - 0.5, max + 0.499999999999, shouldSnap) );
     }
 
     return randInt;
