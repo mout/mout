7272d78416c6f88df427c0f5b7a84656cf3fd058
millermedeiros <miller@millermedeiros.com>
2011-11-24 19:31:22 -0200

    add lang/ctorApply, still missing docs and specs.

---
+++ src/lang/ctorApply.js
@@ -0,0 +1,19 @@
+define(function () {
+
+    function F(){}
+
+    /**
+     * Do fn.apply on a constructor.
+     * @author Miller Medeiros
+     * @version 0.1.0 (2011/11/24)
+     */
+    function ctorApply(ctor, args) {
+        F.prototype = ctor.prototype;
+        var instance = new F();
+        ctor.apply(instance, args);
+        return instance;
+    }
+
+    return ctorApply;
+
+});
