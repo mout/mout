846c3afe242e5ed93cd8cb150d40fcfb760ec02e
millermedeiros <miller@millermedeiros.com>
2011-12-08 03:52:40 -0200

    check if rand/choice argument is an array. closes #24

--- src/random/choice.js
+++ src/random/choice.js
@@ -1,13 +1,13 @@
-define(['./randInt'], function (randInt) {
+define(['./randInt', '../lang/isArray'], function (randInt, isArray) {
 
     /**
      * Returns a random element from the supplied arguments
      * or from the array (if single argument is an array).
      * @author Miller Medeiros
-     * @version 0.1.0 (2011/11/17)
+     * @version 0.1.1 (2011/12/08)
      */
     function choice(items) {
-        var target = (arguments.length === 1 && items.length)? items : arguments;
+        var target = (arguments.length === 1 && isArray(items))? items : arguments;
         return target[ randInt(0, target.length - 1) ];
     }
 
