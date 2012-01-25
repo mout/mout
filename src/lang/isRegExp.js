e7d40d2f3f04bc8e7ec9d0212e96d449f94757b4
André Cruz <andremiguelcruz@msn.com>
2012-01-25 11:18:20 +0000

    Fix typos in isXXX functions.

--- src/lang/isRegExp.js
+++ src/lang/isRegExp.js
@@ -3,8 +3,8 @@ define(['./isKind'], function (isKind) {
      * @author Miller Medeiros
      * @version 0.1.0 (2011/10/31)
      */
-    function isNumber(val) {
+    function isRegExp(val) {
         return isKind(val, 'RegExp');
     }
-    return isNumber;
+    return isRegExp;
 });
