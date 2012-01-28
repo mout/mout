9a9ec3f2ccb5c7b7d0888b1978df3fa42177f6e9
millermedeiros <miller@millermedeiros.com>
2012-01-28 21:24:20 -0200

    change array/insert behavior to accept multiple items at once and return length instead of boolean. see #33

--- tests/spec/array/spec-insert.js
+++ tests/spec/array/spec-insert.js
@@ -2,22 +2,31 @@ define(['src/array/insert'], function (insert) {
 
     describe('array/insert()', function(){
 
-        it('should push and return true if not present', function(){
+        it('should push item if not present and return the length', function(){
 
             var arr = [1, 2, 3];
 
-            expect( insert(arr, 3) ).toBe( false );
+            expect( insert(arr, 3) ).toBe( 3 );
             expect( arr.length ).toBe( 3 );
-            expect( arr[0] ).toBe( 1 );
-            expect( arr[1] ).toBe( 2 );
-            expect( arr[2] ).toBe( 3 );
+            expect( arr ).toEqual( [1,2,3] );
 
-            expect( insert(arr, 4) ).toBe( true );
+            expect( insert(arr, 4) ).toBe( 4 );
             expect( arr.length ).toBe( 4 );
-            expect( arr[0] ).toBe( 1 );
-            expect( arr[1] ).toBe( 2 );
-            expect( arr[2] ).toBe( 3 );
-            expect( arr[3] ).toBe( 4 );
+            expect( arr ).toEqual( [1,2,3,4] );
+        });
+
+        it('should accept multiple items', function () {
+
+            var arr = ['a', 'b'];
+
+            expect( insert(arr, 'a', 'b') ).toBe( 2 );
+            expect( arr.length ).toBe( 2 );
+            expect( arr ).toEqual( ['a', 'b'] );
+
+            expect( insert(arr, 1, 2, 'b', 3, 'a', 'c') ).toBe( 6 );
+            expect( arr.length ).toBe( 6 );
+            expect( arr ).toEqual( ['a', 'b', 1, 2, 3, 'c'] );
+
         });
 
     });
