290a27c795631ade90ac6bb4d054b845e1b5553d
millermedeiros <miller@millermedeiros.com>
2012-01-31 11:32:26 -0200

    refactor object/unset

--- src/object/unset.js
+++ src/object/unset.js
@@ -3,18 +3,15 @@ define(['./has'], function (has) {
     /**
      * Unset object property.
      * @author Miller Medeiros
-     * @version 0.1.0 (2012/01/31)
+     * @version 0.2.0 (2012/01/31)
      */
     function unset(obj, prop){
         if (has(obj, prop)) {
             var parts = prop.split('.'),
                 last = parts.pop();
-
             while (prop = parts.shift()) {
                 obj = obj[prop];
-                if (typeof obj !== 'object') return true;
             }
-
             return (delete obj[last]);
 
         } else {
