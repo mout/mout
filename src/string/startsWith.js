63b61ed1dfa7dfed756e78edd7cb8162ed598e1a
Adam Nowotny <adam.nowotny@gmail.com>
2012-03-01 00:42:39 +1100

    added string/endsWith and string/startsWith

---
+++ src/string/startsWith.js
@@ -0,0 +1,17 @@
+define(function () {
+	/**
+	* Checks if string starts with specified prefix.
+	* @example startsWith('lorem ipsum', 'ipsum') -> false
+	* @example startsWith('lorem ipsum', 'lorem') -> true
+	* @param {string} str
+	* @param {string} prefix
+	* @return {bool}
+	* @version 0.1.0 (2012/03/01)
+	*/
+	function startsWith(str, prefix) {
+		str = (str || '');
+		prefix = (prefix || '');
+		return str.indexOf(prefix) === 0;
+	}
+	return startsWith;
+});
---
+++ tests/spec/string/spec-startsWith.js
@@ -0,0 +1,32 @@
+define(['src/string/startsWith'], function (startsWith) {
+
+    describe('string/startsWith()', function(){
+
+        it('should return true if string starts with prefix', function(){
+            expect( startsWith('lorem-ipsum', 'lorem') ).toBeTruthy();
+        });
+
+        it('should return false if string does not start with prefix', function(){
+        	expect( startsWith('lorem-ipsum', 'ipsum')).toBeFalsy();
+        });
+
+        it('should treat undefined as empty string', function () {
+            var str;
+            expect( startsWith(str, 'ipsum') ).toBeFalsy();
+        });
+
+        it('should treat null as empty string', function(){
+        	expect( startsWith(null, '') ).toBeTruthy();
+        });
+
+        it('should return true if prefix undefined', function(){
+            var prefix;
+            expect( startsWith('lorem-ipsum', prefix) ).toBeTruthy();
+        });
+
+        it('should return true if prefix null', function () {
+        	expect( startsWith('lorem-ipsum', null) ).toBeTruthy();
+        });
+
+    });
+});
