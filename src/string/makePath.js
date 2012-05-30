74dee470b1b77331f001adad1c1f59bf2e4212cc
millermedeiros <miller@millermedeiros.com>
2012-05-30 16:22:02 -0300

    fix string/makePath to avoid uneeded leading slash

--- src/string/makePath.js
+++ src/string/makePath.js
@@ -1,15 +1,19 @@
-define(function(){
+define(['../array/filter'], function(filter){
+
     /**
-     * Group arguments as path segments, if any of the args is `null` it
-     * will be ignored from resulting path.
-     * @example makePath('lorem', 'ipsum', null, 'dolor') -> 'lorem/ipsum/dolor'
-     * @param {...string} args
-     * @return {string}
-     * @version 0.1.0 (2011/07/28)
+     * Group arguments as path segments, if any of the args is `null` or an
+     * empty string it will be ignored from resulting path.
+     * @version 0.2.0 (2012/05/30)
      */
-    function makePath(args){
-        args = Array.prototype.slice.call(arguments);
-        return args.join('/').replace(/\/+/g, '/');
+    function makePath(var_args){
+        var result = filter(Array.prototype.slice.call(arguments), isValidString);
+        return result.join('/').replace(/\/{2,}/g, '/');
     }
+
+    function isValidString(val) {
+        return (typeof val === 'string' && val !== '');
+    }
+
     return makePath;
+
 });
--- tests/spec/string/spec-makePath.js
+++ tests/spec/string/spec-makePath.js
@@ -9,5 +9,26 @@ define(['src/string/makePath'], function (makePath) {
         it('should ignore empty/null values', function(){
             expect( makePath('lorem', null, 'ipsum', '', null, 'dolor') ).toEqual('lorem/ipsum/dolor');
         });
+
+        it('should ignore empty/null values at begin', function () {
+            expect( makePath('', 'foo') ).toEqual( 'foo' );
+            expect( makePath(null, 'bar') ).toEqual( 'bar' );
+            expect( makePath('', null, 'dolor', 'amet') ).toEqual( 'dolor/amet' );
+        });
+
+        it('should keep trailing slash if it exists', function () {
+            expect( makePath('dolor', 'amet/') ).toEqual( 'dolor/amet/' );
+            expect( makePath('dolor', 'ipsum', '/') ).toEqual( 'dolor/ipsum/' );
+        });
+
+        it('should keep leading slash if it exists', function () {
+            expect( makePath('/dolor', 'amet/') ).toEqual( '/dolor/amet/' );
+            expect( makePath('/', 'dolor', 'ipsum', '/') ).toEqual( '/dolor/ipsum/' );
+        });
+
+        it('should remove duplicate slashes', function () {
+            expect( makePath('dolor/', '/',  '/ipsum', '//') ).toEqual( 'dolor/ipsum/' );
+        });
+
     });
 });
