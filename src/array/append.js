5e68b8e4f8ac814dc6cc5ad2229c92b2a6ab6284
millermedeiros <miller@millermedeiros.com>
2012-06-10 11:48:56 -0300

    Improve perf of array/append and fixes "Max call stack size" error. closes #44.

--- src/array/append.js
+++ src/array/append.js
@@ -3,10 +3,15 @@ define(function () {
     /**
      * Appends an array to the end of another.
      * The first array will be modified.
-     * @version 0.1.0 (2012/01/31)
+     * @version 0.1.1 (2012/06/10)
      */
     function append(arr1, arr2) {
-        Array.prototype.push.apply(arr1, arr2);
+        var pad = arr1.length,
+            i = -1,
+            n = arr2.length;
+        while (++i < n) {
+            arr1[pad + i] = arr2[i];
+        }
         return arr1;
     }
     return append;
