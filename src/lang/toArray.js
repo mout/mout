afe2c483341005ecec345db974581ee624cc3e99
millermedeiros <miller@millermedeiros.com>
2012-08-11 00:53:19 -0300

    made lang/toArray support HTMLCollection and other pseudo-array objects. closes #58

--- src/lang/toArray.js
+++ src/lang/toArray.js
@@ -1,20 +1,25 @@
-define(['./isArray', './isObject', './isArguments'], function (isArray, isObject, isArguments) {
+define(function () {
 
     var _win = this;
 
     /**
      * Convert array-like object into array
-     * @version 0.2.0 (2011/12/05)
+     * @version 0.3.0 (2012/08/11)
      */
     function toArray(val){
-        var ret;
+        var ret, n;
 
         if (val == null) {
             ret = [];
-        } else if ( val && val !== _win && (isArray(val) || isArguments(val) || (isObject(val) && 'length' in val)) ) {
+        } else if ( typeof val === 'object' && val !== _win && 'length' in val ) {
             //window returns true on isObject in IE7 and may have length property
             //only convert object to array if it is a array-like object
-            ret = Array.prototype.slice.call(val);
+            // typeof val === 'object' is enough since array is also an object
+            ret = [];
+            n = val.length;
+            while (n--) {
+                ret[n] = val[n];
+            }
         } else {
             //string, regexp, function have .length but user probably just want
             //to wrap value into an array..
--- tests/spec/lang/spec-toArray.js
+++ tests/spec/lang/spec-toArray.js
@@ -33,10 +33,17 @@ define(['src/lang/toArray'], function (toArray) {
             expect( toArray('lorem') ).toEqual( ['lorem'] );
             expect( toArray('') ).toEqual( [''] );
 
+            // avoid string objects it isn't reliable
+            // IE 7-8 can't access chars by index
+            // considered as edge-case and ignored for now
+            // expect( toArray( new String('foo') ) ).toEqual( ['foo'] );
+            // expect( toArray( new String('') ) ).toEqual( [] );
+
             expect( toArray(123) ).toEqual( [123] );
             expect( toArray(0) ).toEqual( [0] );
 
             expect( toArray(/\w+/) ).toEqual( [/\w+/] );
+            expect( toArray( new RegExp('\\w+') ) ).toEqual( [/\w+/] );
 
             expect( toArray(window) ).toEqual( [window] );
             expect( toArray({foo:"bar", lorem:123}) ).toEqual( [{foo:"bar", lorem:123}] );
@@ -44,12 +51,22 @@ define(['src/lang/toArray'], function (toArray) {
             expect( toArray(true) ).toEqual( [true] );
             expect( toArray(false) ).toEqual( [false] );
 
+            var fn = function(){};
+            expect( toArray(fn) ).toEqual( [fn] );
         });
 
         it('should return an empty array if nill value', function () {
             expect( toArray(null) ).toEqual( [] );
             expect( toArray(undefined) ).toEqual( [] );
             expect( toArray() ).toEqual( [] );
+        });
+
+        it('should convert HTMLCollection to real array', function () {
+            var els = document.getElementsByTagName('*');
+            var arr = toArray( els );
+            expect( Object.prototype.toString.call(arr) ).toBe( '[object Array]' );
+            expect( arr.length ).toBe( els.length );
+
         });
 
     });
