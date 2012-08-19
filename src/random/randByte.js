f3ed804dd093cc4a919ed0f6641eead2ee816f72
Jarrod Overson <joverson@onerecovery.com>
2012-08-18 23:15:19 -0700

    missing parens on clarity refactor. Of course.

--- src/random/randByte.js
+++ src/random/randByte.js
@@ -5,8 +5,8 @@ define(function () {
      * @version 0.1.0 (2012/08/18)
      */
     function randomByte() {
-      var byte = ~~(Math.random()*256).toString(16);
-      return ( '0' + byte).substr(-2); // leading zero
+      var byte = (~~(Math.random()*256)).toString(16);
+      return ( '0' + byte).substr(-2);
     }
 
     return randomByte;
