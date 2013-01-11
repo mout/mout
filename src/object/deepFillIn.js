61be0af28a7a23d7cd3353fc32e7c30dea112a3f
Conrad Zimmerman <me@conradz.com>
2013-01-11 14:42:54 -0500

    Only recursively mix plain objects in `object/deepFillIn`

--- src/object/deepFillIn.js
+++ src/object/deepFillIn.js
@@ -1,21 +1,30 @@
-define(['../array/forEach', './forOwn'], function (forEach, forOwn) {
+define(['./forOwn', '../lang/isPlainObject'], function (forOwn, isPlainObject) {
 
     /**
-     * Deeply copy missing properties in the obj from the defaults.
+     * Deeply copy missing properties in the target from the defaults.
      * @version 0.1.0 (2012/12/19)
      */
-    function deepFillIn(obj, var_defaults){
-        forEach(Array.prototype.slice.call(arguments, 1), function(base){
-            forOwn(base, function(val, key){
-                var curVal = obj[key];
-                if (curVal == null) {
-                    obj[key] = val;
-                } else if (typeof curVal === 'object' && typeof val === 'object') {
-                    deepFillIn(curVal, val);
-                }
-            });
-        });
-        return obj;
+    function deepFillIn(target, defaults){
+        var i = 0,
+            n = arguments.length,
+            obj;
+
+        while(++i < n) {
+            obj = arguments[i];
+            if (obj) {
+                forOwn(obj, function(newValue, key) {
+                    var curValue = target[key];
+                    if (curValue == null) {
+                        target[key] = newValue;
+                    } else if (isPlainObject(curValue)
+                               && isPlainObject(newValue)) {
+                        deepFillIn(curValue, newValue);
+                    }
+                });
+            }
+        }
+
+        return target;
     }
 
     return deepFillIn;
