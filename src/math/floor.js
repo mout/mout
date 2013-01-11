f7aaef49f26e4c94543aaec0e437cf24b59ce89c
Friedemann Altrock <frodenius@gmail.com>
2013-01-12 00:00:28 +0100

    Simplify math/floor - no dependency on countSteps

--- src/math/floor.js
+++ src/math/floor.js
@@ -1,11 +1,11 @@
-define(['./countSteps'], function(countSteps){
+define(function(){
     /**
     * Floor value to full steps.
     * @version 0.2.0 (2012/11/30)
     */
     function floor(val, step){
-        step = step || 1;
-        return countSteps(val, step) * step;
+        step = Math.abs(step || 1);
+        return Math.floor(val / step) * step;
     }
     return floor;
 });
