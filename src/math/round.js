e0b809bc018253a7cc0657d80e321ce03c8d1ee4
Friedemann Altrock <frodenius@gmail.com>
2013-01-12 01:46:24 +0100

    Simplify math/round by building on Math.round instead of math/floor

--- src/math/round.js
+++ src/math/round.js
@@ -1,12 +1,11 @@
-define(['./floor'], function (floor) {
-
+define(function () {
     /**
      * Round number to a specific radix
      * @version 0.1.0 (2012/11/27)
      */
     function round(value, radix){
         radix = radix || 1; // default round 1
-        return floor(value + radix / 2, radix);
+        return Math.round(value / radix) * radix;
     }
 
     return round;
