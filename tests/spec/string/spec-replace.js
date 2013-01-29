225d66c49419e6f333f106323650fbef6300eab2
Conrad Zimmerman <me@conradz.com>
2013-01-29 07:24:49 -0500

    Test string/replace using function replacement

--- tests/spec/string/spec-replace.js
+++ tests/spec/string/spec-replace.js
@@ -22,6 +22,20 @@ define(['mout/string/replace'], function(replace){
             expect(result).toEqual('test n n');
         });
 
+        it('should replace with function replacer', function(){
+            function replaceNum(m) {
+                return (+m[0]) * (+m[0]);
+            }
+            function replaceLetter(m) {
+                return m[0].charCodeAt(0);
+            }
+
+            var result = replace('1 2 3 a', [/\d+/g, /[a-z]/g],
+                                 [replaceNum, replaceLetter]);
+
+            expect(result).toEqual('1 4 9 97');
+        });
+
     });
 
 });
