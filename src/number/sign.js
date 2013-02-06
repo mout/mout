d21e496f73ee49eeb63867272f4d25c4f3dfb025
Mathias Paumgarten <mail@mathias-paumgarten.com>
2013-02-06 10:28:10 -0800

    update number/sign to return 0 when value 0

--- src/number/sign.js
+++ src/number/sign.js
@@ -4,6 +4,8 @@ define(function () {
      * Get sign of the value.
      */
     function sign(val) {
+    	if (val === 0) return 0;
+
         return val < 0? -1 : 1;
     }
 
--- tests/spec/number/spec-sign.js
+++ tests/spec/number/spec-sign.js
@@ -9,10 +9,13 @@ define(['mout/number/sign'], function (sign) {
         });
 
         it('should return +1 if number is positive or zero', function () {
-            expect( sign(0) ).toEqual(1);
             expect( sign(123) ).toEqual(1);
             expect( sign(0.5) ).toEqual(1);
             expect( sign(Math.pow(2,32)) ).toEqual(1);
+        });
+
+        it('should return 0 if number is 0', function () {
+            expect( sign(0) ).toEqual(0);
         });
 
     });
