b9bd164e7e5247b180be95e5dbba224bfe7373b2
Conrad Zimmerman <me@conradz.com>
2013-02-07 08:37:01 -0500

    Use jasmine.Clock to mock the JS clock for timing tests

--- tests/spec/function/spec-throttle.js
+++ tests/spec/function/spec-throttle.js
@@ -2,43 +2,42 @@ define(['mout/function/throttle'], function(throttle){
 
     describe('function/throttle', function(){
 
-        // IMPORTANT: setTimeout behavior can be very misleading (specially in
-        // FF18 and IE 7-8) so we avoid using it on the tests.
+        var timerCallback;
+
+        beforeEach(function() {
+            timerCallback = jasmine.createSpy('timerCallback');
+            jasmine.Clock.useMock();
+        });
 
         it('should execute callback only once per interval', function(){
-            var count = 0;
-            var start = new Date();
-            var cb = throttle(function(){
-                count++;
-            }, 50);
-            var limit = 120;
-            while((new Date() - start) < limit) {
-                cb();
-            }
-            // we compare using greaterThan/lessThan to avoid issues on
-            // travis-ci (see #41)
-            expect( count ).toBeGreaterThan( 1 );
-            expect( count ).toBeLessThan( 4 );
+            var cb = throttle(timerCallback, 50);
+            var i = 0;
+
+            while (++i <= 5) { cb(); }
+            expect(timerCallback.callCount).toEqual(1);
+
+            jasmine.Clock.tick(51);
+
+            i = 0;
+            while (++i <= 5) { cb(); }
+            expect(timerCallback.callCount).toEqual(2);
         });
 
 
         it('should allow passing args and should use first supplied value by default', function () {
             var count = 0;
-            var cb = throttle(function(a, b){
-                count += a + b;
+            var cb = throttle(function(x, y) {
+                count += x + y;
             }, 50);
-            var start = new Date();
-            var limit = 120;
-            cb(1,2);
-            cb(3,4);
-            expect( count ).toBe( 3 );
-            while ((new Date() - start) < limit) {
-                cb(1,1);
-            }
-            // we compare using greaterThan/lessThan to avoid issues on
-            // travis-ci (see #41)
-            expect( count ).toBeGreaterThan( 3 );
-            expect( count ).toBeLessThan( 8 );
+
+            var i = 0;
+            cb(1, 2);
+            cb(3, 4);
+            cb(5, 6);
+
+            expect(count).toEqual(3);
+            jasmine.Clock.tick(51);
+            expect(count).toEqual(14);
         });
 
         it('throttled fn should always return first returned value', function () {
