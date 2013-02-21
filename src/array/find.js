824f1e172ad5bac438757bc2face86fa15e3efe6
millermedeiros <miller@millermedeiros.com>
2013-02-21 12:50:53 -0300

    simplify array/find by reusing array/findIndex

--- src/array/find.js
+++ src/array/find.js
@@ -1,21 +1,11 @@
-define(['./some', '../function/makeIterator_'], function (some, makeIterator) {
+define(['./findIndex'], function (findIndex) {
 
     /**
      * Returns first item that matches criteria
      */
     function find(arr, iterator, thisObj){
-        iterator = makeIterator(iterator);
-        var needle,
-            i = -1, n = arr.length,
-            val;
-        while (++i < n){
-            val = arr[i];
-            if (iterator.call(thisObj, val, i, arr)) {
-                needle = val;
-                break;
-            }
-        }
-        return needle;
+        var idx = findIndex(arr, iterator, thisObj);
+        return idx >= 0? arr[idx] : void(0);
     }
 
     return find;
