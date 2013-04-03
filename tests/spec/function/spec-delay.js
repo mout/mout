43340aef673a9aa94692a0345a62afba19d59f74
Mathias Paumgarten <mail@mathias-paumgarten.com>
2013-04-03 23:28:35 +0200

    updates spec-delay.js to use jasmine.Clock

--- tests/spec/function/spec-delay.js
+++ tests/spec/function/spec-delay.js
@@ -10,116 +10,69 @@ define(['mout/function/delay'], function (delay) {
         	this.a = value;
         }
 
+        beforeEach(function() {
+            jasmine.Clock.useMock();
+        });
+
         it('should delay execution', function(){
-            runs(function(){
-                this.callback = jasmine.createSpy();
+            var callback = jasmine.createSpy();
+            delay(callback, 300);
 
-                delay(this.callback, 300);
-            });
+            jasmine.Clock.tick(100);
+            expect(callback).not.toHaveBeenCalled();
 
-            waits(100);
-
-            runs(function(){
-                expect(this.callback).not.toHaveBeenCalled();
-            });
-
-            waits(250);
-
-            runs(function(){
-                expect(this.callback).toHaveBeenCalled();
-            });
+            jasmine.Clock.tick(250);
+            expect(callback).toHaveBeenCalled();
         });
 
         it('should call closure with context', function(){
-        	runs(function(){
-        		this.context = { a: 0 };
+        	var context = { a: 0 };
+            delay(doIt, 300, context);
 
-                delay(doIt, 300, this.context);
-            });
-
-            waits(350);
-
-            runs(function(){
-                expect(this.context.a).toBe(1);
-            });
+            jasmine.Clock.tick(350);
+            expect(context.a).toBe(1);
         });
 
         it('should curry arguments', function(){
-        	runs(function(){
-        		this.context = { a: 0 };
+    		var context = { a: 0 };
+    		delay(manipulate, 300, context, [ 5 ]);
 
-        		delay(manipulate, 300, this.context, [ 5 ]);
-        	});
-
-        	waits(350);
-
-        	runs(function(){
-        		expect(this.context.a).toBe(5);
-        	});
+            jasmine.Clock.tick(350);
+    		expect(context.a).toBe(5);
         });
 
         it('should override previous delay by default', function(){
+            var callback = jasmine.createSpy();
+            delay(callback, 200);
 
-            runs(function(){
-                this.callback = jasmine.createSpy();
+            jasmine.Clock.tick(100);
+            delay(callback, 200);
 
-                delay(this.callback, 200);
-            });
-
-            waits(100);
-
-            runs(function(){
-                delay(this.callback, 200);
-            })
-
-            waits(300);
-
-            runs(function(){
-                expect(this.callback.callCount).toBe(1);
-            });
+            jasmine.Clock.tick(300);
+            expect(callback.callCount).toBe(1);
         });
 
         it('should not override delay', function(){
-            runs(function(){
-                this.callback = jasmine.createSpy();
+            var callback = jasmine.createSpy();
+            delay(callback, 300);
 
-                delay(this.callback, 300);
-            });
+            jasmine.Clock.tick(100);
+            delay(callback, 100, this, null, false);
 
-            waits(100);
-
-            runs(function(){
-                delay(this.callback, 100, this, null, false);
-            });
-
-            waits(250);
-
-            runs(function(){
-                expect(this.callback.callCount).toBe(2);
-            })
-
+            jasmine.Clock.tick(250);
+            expect(callback.callCount).toBe(2);
         });
 
         it('should clear an delay', function() {
-            runs(function() {
-                this.callback = jasmine.createSpy();
 
-                this.identifier = delay(this.callback, 300);
-            });
+            var callback = jasmine.createSpy();
+            var identifier = delay(callback, 300);
 
-            waits(100);
+            jasmine.Clock.tick(100);
+            delay.clear(identifier);
 
-            runs(function() {
-                delay.clear(this.identifier);
-            });
-
-            waits(250);
-
-            runs(function() {
-                expect(this.callback).not.toHaveBeenCalled();
-            });
-
+            jasmine.Clock.tick(250);
+            expect(callback).not.toHaveBeenCalled();
         });
-
     });
 });
