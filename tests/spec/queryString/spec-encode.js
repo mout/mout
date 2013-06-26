f7d6c13d0c282525a1d5a0437bdc374391bdaca6
igor <igor.p.almeida@gmail.com>
2013-06-26 15:41:47 -0700

    spec test changed to cover empty array and array with empty values

--- tests/spec/queryString/spec-encode.js
+++ tests/spec/queryString/spec-encode.js
@@ -12,15 +12,6 @@ define(['mout/queryString/encode'], function (encode) {
             expect( encode(q) ).toBe( '?a=123&b=false&c=null&d=bar' );
         });
 
-        it('should run through Array values.', function () {
-            var q = {
-                a : "foo",
-                b : [0,false,null,undefined,'spéçîãl çhârs'],
-                c : []
-            };
-            expect( encode(q) ).toBe( '?a=foo&b=0&b=false&b=null&b=undefined&b=sp%C3%A9%C3%A7%C3%AE%C3%A3l%20%C3%A7h%C3%A2rs&c=' );
-        });
-
         it('should encode special chars', function () {
             var q = {
                 a : 'bar',
@@ -29,6 +20,17 @@ define(['mout/queryString/encode'], function (encode) {
             };
             expect( encode(q) ).toBe( '?a=bar&b=lorem%20ipsum&c=sp%C3%A9%C3%A7%C3%AE%C3%A3l%20%C3%A7h%C3%A2rs' );
         });
+
+        it('should run through Array values.', function () {
+            var q = {
+                a : "foo",
+                b : [0,false,null,undefined,'spéçîãl çhârs'],
+                c : [],
+                e : ['', '', "foo"]
+            };
+            expect( encode(q) ).toBe( '?a=foo&b=0&b=false&b=null&b=undefined&b=sp%C3%A9%C3%A7%C3%AE%C3%A3l%20%C3%A7h%C3%A2rs&c=&e=&e=&e=foo' );
+        });
+
     });
 
 });
