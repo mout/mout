09e36bd5416ce7db1baa57767bdb55e8d1ddc626
igor <igor.p.almeida@gmail.com>
2013-06-26 12:48:09 -0700

    tests considering special chars and  arrays with empty values.

--- tests/spec/queryString/spec-decode.js
+++ tests/spec/queryString/spec-decode.js
@@ -26,9 +26,11 @@ define(['mout/queryString/decode'], function (decode) {
         });
 
         it('should allow properties with same name and create an array', function () {
-            var q = decode('?a=undefined&a=false&a=0&a=null&a=bar&b=123&b=foo');
-            expect( q.a ).toEqual( [undefined,false,0,null,"bar"] );
-            expect( q.b ).toEqual( [123,"foo"] );
+            var q = decode('?a=undefined&a=false&a=0&a=null&a=bar&b=123&b=foo&c=loren&d=&d=&d=sp%C3%A9%C3%A7%C3%AE%C3%A3l%20%C3%A7h%C3%A2rs');
+            expect( q.a ).toEqual( [undefined, false, 0, null, 'bar'] );
+            expect( q.b ).toEqual( [123, 'foo'] );
+            expect( q.c ).toEqual( 'loren' );
+            expect( q.d ).toEqual( ['', '', 'spéçîãl çhârs'] );
         });
 
     });
