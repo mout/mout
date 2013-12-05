dc6c7e219caf5a59803d1b5e0cb44cbdc620f589
Conrad Zimmerman <me@conradz.com>
2013-12-05 15:15:02 +0000

    Parse numeric character references in unescapeHtml

--- tests/spec/string/spec-unescapeHtml.js
+++ tests/spec/string/spec-unescapeHtml.js
@@ -7,6 +7,25 @@ define(['mout/string/unescapeHtml'], function (unescapeHtml) {
                 .toEqual( '<em>\'lorem\'</em> & "ipsum"' );
         });
 
+        it('should convert decimal character references into chars', function() {
+            expect( unescapeHtml('&#9;&#10;&#010;&#0010;') )
+                .toEqual( '\t\n\n\n' );
+        });
+
+        it('should convert hexadecimal character references into chars', function() {
+            expect( unescapeHtml('&#x2b;&#X2b;&#x02B;&#x002b;') )
+                .toEqual( '++++' );
+        });
+
+        it('should ignore invalid escapes', function() {
+            expect( unescapeHtml('&test;') ).toEqual( '&test;' );
+            expect( unescapeHtml('&12') ).toBe( '&12' );
+            expect( unescapeHtml('&x12') ).toBe( '&x12' );
+            expect( unescapeHtml('amp;') ).toBe( 'amp;' );
+            expect( unescapeHtml('&;') ).toBe( '&;' );
+            expect( unescapeHtml('&') ).toBe( '&' );
+        });
+
         it('should return empty string if no argument', function () {
             expect( unescapeHtml() ).toBe( '' );
         });
