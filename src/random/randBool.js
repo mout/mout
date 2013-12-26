c3102d0e9a90a3f1de0040e3fa13eb0ffe53e9d3
millermedeiros <miller@millermedeiros.com>
2013-12-26 10:13:00 -0200

    use `>= 0.5` instead of `> 0.5` on randBool
    `random()` lower-bound is zero inclusive and upper-bound is one exclusive; and
    `0.5` usually rounds up.

--- src/random/randBool.js
+++ src/random/randBool.js
@@ -4,7 +4,7 @@ define(['./random'], function (random) {
      * returns a random boolean value (true or false)
      */
     function randBool(){
-        return random() > 0.5;
+        return random() >= 0.5;
     }
 
     return randBool;
