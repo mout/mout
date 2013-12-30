f4948d65f1f12e36acf4ae9dcba37813c006ae65
Conrad Zimmerman <me@conradz.com>
2013-12-30 14:39:02 +0000

    Use custom array/slice implementation
    This fixes issues with NodeList objects in IE8
    
    closes #159. closes #161

--- src/array/slice.js
+++ src/array/slice.js
@@ -6,7 +6,19 @@ define(function () {
      * Create slice of source array or array-like object
      */
     function slice(arr, start, end){
-        return arrSlice.call(arr, start, end);
+        if (start == null) {
+            start = 0;
+        }
+        if (end == null) {
+            end = arr.length;
+        }
+
+        var result = [];
+        while (start < end) {
+            result.push(arr[start++]);
+        }
+
+        return result;
     }
 
     return slice;
