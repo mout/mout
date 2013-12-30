8bb96790c0e6624a28061cfcc0c3729fe50c21ee
n3okill <n3okill@hotmail.com>
2013-12-30 21:30:33 +0000

    Performance improvement string/repeat

--- src/string/repeat.js
+++ src/string/repeat.js
@@ -1,11 +1,23 @@
-define(['../lang/toString'], function(toString){
+define(['../lang/toString', '../number/toInt'], function(toString, toInt){
 
     /**
      * Repeat string n times
+     *
+     * Performance improvement based on http://stackoverflow.com/questions/202605/repeat-string-javascript
+     * Perf tests: http://jsfiddle.net/disfated/GejWV/
      */
      function repeat(str, n){
+         var result = '';
          str = toString(str);
-         return (new Array(n + 1)).join(str);
+         n = toInt(n);
+         if(n < 1) return '';
+         while(n > 0) {
+             //Examples of bitwise operators can be seen at
+             //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
+             if(n & 1) result += str;
+             n >>= 1, str += str;
+         };
+         return result;
      }
 
      return repeat;
