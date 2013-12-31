2606c61f508a2a98ec70bc932d1014cd9e3e124f
n3okill <n3okill@hotmail.com>
2013-12-31 23:09:14 +0000

    remake repeat code, simpler to read now, using normal math operations instead of bitwise operators. The operation is exactly the same with or without the bitwise.

--- src/string/repeat.js
+++ src/string/repeat.js
@@ -10,14 +10,13 @@ define(['../lang/toString', '../number/toInt'], function(toString, toInt){
          var result = '';
          str = toString(str);
          n = toInt(n);
-         if(n < 1) return '';
-         while(n > 0) {
-             //Examples of bitwise operators can be seen at
-             //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
-             if(n & 1) result += str;
-             n >>= 1, str += str;
-         };
-         return result;
+        if (n < 1) return '';
+        while (n > 0) {
+            if (n % 2) result += str;
+            n = Math.floor(n / 2);
+            str += str;
+        }
+        return result;
      }
 
      return repeat;
