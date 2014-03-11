06218a1c47a3f01c15da8ac3a3f10fabff042752
Miller Medeiros <miller@millermedeiros.com>
2014-03-11 10:23:17 -0300

    fix array/slice behavior when start/end is higher than array length. see #170

--- src/array/slice.js
+++ src/array/slice.js
@@ -4,16 +4,22 @@ define(function () {
      * Create slice of source array or array-like object
      */
     function slice(arr, start, end){
+        var len = arr.length;
+
         if (start == null) {
             start = 0;
         } else if (start < 0) {
-            start = Math.max(arr.length + start, 0);
+            start = Math.max(len + start, 0);
+        } else {
+            start = Math.min(start, len);
         }
 
         if (end == null) {
-            end = arr.length;
+            end = len;
         } else if (end < 0) {
-            end = Math.max(arr.length + end, 0);
+            end = Math.max(len + end, 0);
+        } else {
+            end = Math.min(end, len);
         }
 
         var result = [];
