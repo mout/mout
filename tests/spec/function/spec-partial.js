93c01d110be4802ce1491cf137e35e17978b8eaa
elclanrs <elclanrs@gmail.com>
2014-05-10 02:06:09 -0400

    partial with placeholder

--- tests/spec/function/spec-partial.js
+++ tests/spec/function/spec-partial.js
@@ -1,7 +1,13 @@
 define(['mout/function/partial'], function(partial){
 
+    var _ = undefined;
+
     var add = function(a, b){
         return a + b;
+    };
+
+    var append = function(a, b, c){
+      return a + b + c;
     };
 
 
@@ -15,6 +21,14 @@ define(['mout/function/partial'], function(partial){
 
         it('should curry multiple arguments', function(){
             expect( partial(add, 10, 5)() ).toBe( 15 );
+        });
+
+        it('should partially apply an argument with placeholder', function(){
+            expect( partial(append, _, 'b', 'c')('a') ).toBe( 'abc' );
+        });
+
+        it('should partially apply multiple arguments with placeholder', function(){
+            expect( partial(append, _, _, 'c')('a', 'b') ).toBe( 'abc' );
         });
 
     });
