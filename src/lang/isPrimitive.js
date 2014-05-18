34376e30c642a64dc47add97950f777bd2add8ac
Garrick Cheung <garrick@garrickcheung.com>
2014-05-18 13:36:14 -0700

    Add isPrimitive.

---
+++ src/lang/isPrimitive.js
@@ -0,0 +1,19 @@
+define(function () {
+
+    /**
+     * Checks if the object is a primitive
+     */
+    function isPrimitive(obj) {
+        switch (typeof obj) {
+            case "string":
+            case "number":
+            case "boolean":
+                return true;
+        }
+
+        return obj === null || obj === undefined;
+    }
+
+    return isPrimitive;
+
+});
