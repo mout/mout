e269447c274d4f246e89b4b00ffeb8eafd8d0453
Diego Leme <diego.leme@99taxis.com>
2015-04-21 17:42:40 -0300

    Test: should extract query string from url with hash

--- tests/spec/queryString/spec-getQuery.js
+++ tests/spec/queryString/spec-getQuery.js
@@ -6,6 +6,11 @@ define(['mout/queryString/getQuery'], function (getQuery) {
             var q = getQuery("http://example.com/?foo=bar&a=123&b=false&c=null");
             expect( q ).toBe( '?foo=bar&a=123&b=false&c=null' );
         });
+
+        it('should extract query string from url with hash', function () {
+            var q = getQuery("http://example.com/#/with-hash/?foo=bar&a=123&b=false&c=null");
+            expect( q ).toBe( '?foo=bar&a=123&b=false&c=null' );
+        });
     });
 
 });
