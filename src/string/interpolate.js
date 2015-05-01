9998d0404b2b1bee023c24ba4214f8227474e732
vikram <vikikamath@gmail.com>
2015-04-30 23:00:04 -0400

    Updated string/interpolate to recursively interpolate deeply nested templates

--- src/string/interpolate.js
+++ src/string/interpolate.js
@@ -11,7 +11,8 @@ define([
     function interpolate(template, replacements, syntax){
         template = toString(template);
         var replaceFn = function(match, prop){
-            return toString( get(replacements, prop) );
+            var template = toString( get(replacements, prop) );
+            return stache.test(template) ? template.replace(syntax || stache, replaceFn) :toString( get(replacements, prop) );
         };
         return template.replace(syntax || stache, replaceFn);
     }
