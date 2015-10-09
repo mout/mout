588be08f87ee06cc0e14aa1fdb4da3c1417949a4
Mathias Paumgarten <mail@mathias-paumgarten.com>
2015-10-09 13:49:00 -0700

    v0.11.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /**@license
- * mout v0.11.0 | http://moutjs.com | MIT license
+ * mout v0.11.1 | http://moutjs.com | MIT license
  */
 define(function(require){
 
 //automatically generated, do not edit!
 //run `node build` instead
 return {
-    'VERSION' : '0.11.0',
+    'VERSION' : '0.11.1',
     'array' : require('./array'),
     'collection' : require('./collection'),
     'date' : require('./date'),
