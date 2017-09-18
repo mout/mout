d3cca9554d31b7e20dd9b76eae463b532a2b7767
Max Nordlund <max.nordlund@gmail.com>
2017-09-18 23:04:20 +0200

    Address comment about closure variable lookup.

--- src/lang/kindOf.js
+++ src/lang/kindOf.js
@@ -1,12 +1,9 @@
 define(function () {
-
-    var _toString = Object.prototype.toString;
-
     /**
      * Gets the "kind" of value. (e.g. "String", "Number", etc)
      */
     function kindOf(val) {
-        return _toString.call(val).slice(8, -1);
+        return Object.prototype.toString.call(val).slice(8, -1);
     }
     return kindOf;
 });
