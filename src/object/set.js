21bc58f45ad030bf6ff74106b6460cde68b70070
arnaudNYC <adostes@gmail.com>
2021-03-19 14:48:51 -0400

    Security Fix for Prototype Pollution

--- src/object/set.js
+++ src/object/set.js
@@ -4,6 +4,10 @@ define(['./namespace'], function (namespace) {
      * set "nested" object property
      */
     function set(obj, prop, val){
+        // prototype pollution mitigation
+        if(prop.includes('__proto__') || prop.includes('prototype') || prop.includes('constructor')) {
+            return false;
+        }
         var parts = (/^(.+)\.(.+)$/).exec(prop);
         if (parts){
             namespace(obj, parts[1])[parts[2]] = val;
